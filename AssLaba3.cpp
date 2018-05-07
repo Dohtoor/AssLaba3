107fc22a775a70bb01b09d46b050f2b13320606f
Dohtoor <antroart12@gmail.com>
2018-05-07 20:40:02 +0300

    help

---
+++ AssLaba3.cpp
@@ -0,0 +1,25 @@
+// AssLaba3.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+int main()
+{
+	char a = 29, b = 18, c = 31, d = -14;
+
+	__asm {
+		not b
+			mov al, b
+			and al, c
+			or al, a
+			and al, d
+			mov b, al
+	}
+
+	std::cout << std::hex << static_cast<int>(b) << "h" << std::endl;
+	system("pause");
+	return 0;
+}
+
